b076dc4a00a476c91441d2da3d21f059b83905a5
Prajnakalpa Mishra <56736266+prajnakalpa@users.noreply.github.com>
2026-04-09 02:22:28 +0530

    Add remote image patterns to next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: 'thinqtank.co.in' },
+    ],
+  },
+}
+
+module.exports = nextConfig;
