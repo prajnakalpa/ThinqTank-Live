60cb62602c61f75442521c0cd99e1e9a429b39c1
Prajnakalpa Mishra <56736266+prajnakalpa@users.noreply.github.com>
2026-04-09 18:06:20 +0530

    Update page.tsx

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -1,4 +1,8 @@
 'use client'
+export const dynamic = 'force-dynamic'
+
+
+
 import { useEffect, useState, useCallback, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase/client'
