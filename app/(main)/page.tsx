94bc3705c2f9b553df0774eb411fb29c6e05a4ae
Prajnakalpa Mishra <56736266+prajnakalpa@users.noreply.github.com>
2026-04-10 01:39:02 +0530

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,7 +2,7 @@
 export const dynamic = 'force-dynamic'
 
 
-import { createClient } from '@/lib/supabase/client'
+import { createClient } from '@/lib/supabase/server'
 import Link from 'next/link'
 
 async function getCMSContent() {
